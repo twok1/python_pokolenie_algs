b1aabed391ab9f048e7bb6a0da21acf1e5253f26
twok1 <twokey25@gmail.com>
2025-12-08 15:37:17 +0300

    0812

--- p2-7.py
+++ p2-7.py
@@ -6,12 +6,24 @@ def polynomial_sum(p1, p2):
     first_non_zero = next((i for i, x in enumerate(result) if x != 0), 0)
     return result[first_non_zero:]
 
-p1 = (1, 7, 0, -4)               # P1(x) = x^3 + 7x^2 - 4
-p2 = (-1, 0, 0, 2)               # P2(x) = -x^3 + 2
+import re
 
-print(polynomial_sum(p1, p2))    # P1(x) + P2(x) = 7x^2 - 2
+def polynomial(p):
+    result = []
+    
+    def argument(arg, degree):
+        if arg == 0 and degree == 0:
+            return ''
+        elif degree == 0:
+            return str(arg) if arg < 0 else f'+{arg}'
+        elif arg == 1:
+            return 
+    
+    for num, arg in enumerate(p):
+        degree = len(p) - num
+        
+        
 
-p1 = (1, 2, 3, 4, 5)             # P1(x) = x^4 + 2x^3 + 3x^2 + 4x + 5
-p2 = (1,)                        # P2(x) = 1
-
-print(polynomial_sum(p1, p2))    # P1(x) + P2(x) = x^4 + 2x^3 + 3x^2 + 4x + 6
+print(polynomial((-1, -1)))
+print(polynomial((1, 3, -1, 1, -2)))
+print(polynomial((1, 1, 1)))
