515be02ab9525990f8062c54ec673b9213972d34
twok1 <twokey25@gmail.com>
2026-04-29 11:36:55 +0300

    2904

--- p4-2.py
+++ p4-2.py
@@ -107,14 +107,15 @@ def make_palindrome(n):
     def get_i_num(num, i):
         return num // (10 ** i) % 10
     def check_palindrome(num):
-        for i in range(ceil(log10(num)/2)):
+        for i in range(ceil(log10(num)/2) - 1, -1, -1):
             print(get_i_num(num, i))
+        return True
     k = 0
     def recur(n):
         
         if k == 5:
             return -1
-    check_palindrome(n)
+    print(check_palindrome(n))
 
 make_palindrome(121)
 make_palindrome(1221)
